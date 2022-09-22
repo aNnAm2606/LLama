9bd094c2b6c792cac9e470a931fcb43391a4aa00
aNnAm2606 <73582949+aNnAm2606@users.noreply.github.com>
2022-09-22 22:24:56 +0200

    Premake

---
+++ premake5.lua
@@ -0,0 +1,110 @@
+workspace "Llama"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+
+    outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Llama"
+    location "Llama"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files 
+    {
+        "%{prj.name}/srs/**.h",
+        "%{prj.name}/srs/**.cpp"
+    }
+
+    includedirs
+    {
+         "%{prj.name}//vendor/spdlog/include;"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "10.0.18362.0"
+
+        defines
+        {
+            "LLAMA_PLATFORM_WINDOWS",
+            "LLAMA_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+
+   filter "configurations:Debug"
+       defines "LLAMA_DEBUG"
+       symbols "On"
+         
+   filter "configurations:Release"
+        defines "LLAMA_RELEASE"
+        optimize "On"
+
+   filter "configurations:Dist"
+        defines "LLAMA_DIST"
+        optimize "On"
+                       
+
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files 
+    {
+        "%{prj.name}/srs/**.h",
+        "%{prj.name}/srs/**.cpp"
+    }
+
+   
+    includedirs
+    {
+         "Llama/vendor/spdlog/include;",
+         "Llama/src"
+    }
+
+    
+    links 
+    {
+        "Llama"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "10.0.18362.0"
+
+        defines
+        {
+            "LLAMA_PLATFORM_WINDOWS"
+        }
+
+   filter "configurations:Debug"
+        defines "LLAMA_DEBUG"
+        symbols "On"
+
+   filter "configurations:Release"
+        defines "LLAMA_RELEASE"
+        optimize "On"
+
+   filter "configurations:Dist"
+        defines "LLAMA_DIST"
+        optimize "On"
+                   
