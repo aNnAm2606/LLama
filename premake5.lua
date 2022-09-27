d22ea680e9278cdcc3a8c422b91a6e08a3acfd09
aNnAm2606 <73582949+aNnAm2606@users.noreply.github.com>
2022-09-27 12:54:44 +0200

    Premake file modified

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,6 @@
 workspace "Llama"
     architecture "x64"
+    startproject "Sandbox"
 
     configurations
     {
@@ -67,7 +68,7 @@ project "Llama"
 
         postbuildcommands
 		{
-			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
 		}
 
    filter "configurations:Debug"
